5d933fd12aa5b8d0b08cd3d55206bc5cc863c7eb
libo <jianglibo@gmail.com>
2016-02-01 20:04:24 +0800

    update jsdoc

--- test/index.js
+++ test/index.js
@@ -1,25 +1,19 @@
 var assert = require('assert');
-var AsciiTree = require('../index').AsciiTree;
-var Convertor = require('../index').Convertor;
-var TreeLine = require('../index').TreeLine;
-var BytesLine = require('../index').BytesLine;
-var LineUtil = require('../index').LineUtil;
-var AsciiTreeBuilder = require('../index').AsciiTreeBuilder;
-var ConvertorBuilder = require('../index').ConvertorBuilder;
-var Char = require('../index').Char;
+
+var asciitree = require('../lib/index');
 
 
 describe('required', function() {
   describe('#require()', function() {
     it('should ok', function() {
-      assert(AsciiTree, "AsciiTree should imported.");
-      assert(Convertor, "Convertor should imported.");
-      assert(TreeLine, "Line should imported.");
-      assert(LineUtil, "LineUtil should imported.");
-      assert(Char, "Char should imported.");
-      assert(BytesLine, "BytesLine should imported.");
-      assert(AsciiTreeBuilder, "AsciiTreeBuilder should imported.");
-      assert(ConvertorBuilder, "ConvertorBuilder should imported.");
+      assert(asciitree.AsciiTree, "AsciiTree should imported.");
+      assert(asciitree.Convertor, "Convertor should imported.");
+      assert(asciitree.TreeLine, "Line should imported.");
+      assert(asciitree.LineUtil, "LineUtil should imported.");
+      assert(asciitree.Char, "Char should imported.");
+      assert(asciitree.BytesLine, "BytesLine should imported.");
+      assert(asciitree.AsciiTreeBuilder, "AsciiTreeBuilder should imported.");
+      assert(asciitree.ConvertorBuilder, "ConvertorBuilder should imported.");
     });
   });
 });
